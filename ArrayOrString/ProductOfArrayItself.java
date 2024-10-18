3961bb7140a60f335d50cc3e0bd34ce656d32d3b
susantacemk <cse21083@cemk.ac.in>
2024-10-18 12:51:33 +0530

    https://leetcode.com/problems/product-of-array-except-self/description/?envType=study-plan-v2&envId=leetcode-75

---
+++ ArrayOrString/ProductOfArrayItself.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class ProductOfArrayItself {
+    public static void main(String[] args) {
+        int[] ans = productExceptSelf(new int[] { 1, 2, 3, 4 });
+        System.out.println(Arrays.toString(ans));
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+
+        left[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+
+        right[right.length - 1] = 1;
+        for (int i = right.length - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = left[i] * right[i];
+        }
+        return result;
+    }
+}
