46549511a3918f71f1e86f9e52df235ffe471ae0
susantacemk <cse21083@cemk.ac.in>
2024-10-18 12:54:55 +0530

    updated

--- ArrayOrString/ProductOfArrayItself.java
+++ ArrayOrString/ProductOfArrayItself.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 
+// https://leetcode.com/problems/product-of-array-except-self/description/?envType=study-plan-v2&envId=leetcode-75
 public class ProductOfArrayItself {
     public static void main(String[] args) {
         int[] ans = productExceptSelf(new int[] { 1, 2, 3, 4 });
