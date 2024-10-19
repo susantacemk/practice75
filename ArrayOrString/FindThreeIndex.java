aa3d309a79b3b938b701ae11803d39c26f6b966a
susantacemk <cse21083@cemk.ac.in>
2024-10-19 16:47:55 +0530

    https://leetcode.com/problems/increasing-triplet-subsequence/?envType=study-plan-v2&envId=leetcode-75

--- ArrayOrString/FindThreeIndex.java
+++ ArrayOrString/FindThreeIndex.java
@@ -1,6 +1,7 @@
 public class FindThreeIndex {
     public static void main(String[] args) {
         System.out.println(increasingTriplet(new int[] { 1, 2, 3, 4, 5 }));
+        System.out.println(increasingTriplet(new int[] { 2, 1, 5, 0, 4, 6 }));
     }
 
     public static boolean increasingTriplet(int[] nums) {
